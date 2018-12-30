00d6c7ed5dba8626dacd97ea518570c8bc229b8a
Felix Rossmann <rossmann@cs.uni-bonn.de>
2018-12-30 19:55:00 +0100

    Add Logger (NLog) and command argument parsing.

--- Artifact-Detector/Program.cs
+++ Artifact-Detector/Program.cs
@@ -1,15 +1,79 @@
-﻿using System;
+﻿/**
+ * Written by Felix Rossmann, "rossmann@cs.uni-bonn.de".
+ * 
+ * For license, please see "License-LGPL.txt".
+ */
+
+using Microsoft.Extensions.Logging;
+using Mono.Options;
+using NLog.Config;
+using NLog.Extensions.Logging;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArtifactDetector
 {
     class Program
     {
+        private static void ShowHelp(OptionSet options)
+        {
+            // show some app description message
+            Console.WriteLine("Usage: Artifact-Detector.exe [OPTIONS]+");
+            Console.WriteLine("Takes the supplied screenshot and looks in it for the artifact specified.");
+            Console.WriteLine();
+
+            // output the options
+            Console.WriteLine("Options:");
+            options.WriteOptionDescriptions(Console.Out);
+        }
+
         static void Main(string[] args)
         {
+            // Setup logging.
+            ILoggerProvider loggerProvider = new NLogLoggerProvider();
+            ILoggerFactory loggerFactory = new LoggerFactory();
+            loggerFactory.AddProvider(loggerProvider);
+            var logger = loggerFactory.CreateLogger("Main");
+
+            // Setup command line arguments.
+            var logVerbosity = 0;
+            var screenshotPath = "";
+            var artifactGoal = "";
+            var shouldShowHelp = false;
+
+            var options = new OptionSet
+            {
+                { "s|screenshot=", "the path to the screenshot to search in.", p => screenshotPath = p },
+                { "a|artifact=", "name of the artifact to look for.", a => artifactGoal = a },
+                { "v", "increase log message verbosity", v => {
+                    if (v != null)
+                        ++logVerbosity;
+                } },
+                { "h|help", "show this message and exit", h => shouldShowHelp = h != null }
+            };
+
+            // Parse the command line.
+            logger.LogDebug("Try to parse the command line");
+            try
+            {
+                options.Parse(args);
+            } catch (OptionException e)
+            {
+                logger.LogError("Error ocurred while parsing the options: {0} with option {1}.", e.Message, e.OptionName);
+                Console.Error.Write("Error: " + e.Message);
+                shouldShowHelp = true;
+            }
+
+            // Should we show the help?
+            if (shouldShowHelp || string.IsNullOrEmpty(screenshotPath) || string.IsNullOrEmpty(artifactGoal))
+            {
+                ShowHelp(options);
+                return;
+            }
+
+            // Launch actual program.
+            logger.LogInformation("We got the path {screenshotPath} and the artifact goal {artifactGoal}.", screenshotPath, artifactGoal);
+            logger.LogDebug("Call the actual comparison.");
         }
     }
 }
