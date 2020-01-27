2091ed1de64586f9509a90b97a4af86375cc0ea1
Felix Rossmann <rossmann@cs.uni-bonn.de>
2020-01-27 17:38:36 +0100

    Restore main function of program

--- Arbitrary-Artifact-Detector/Program.cs
+++ Arbitrary-Artifact-Detector/Program.cs
@@ -1,7 +1,5 @@
-﻿using ArbitraryArtifactDetector.Model;
-using ArbitraryArtifactDetector.Service;
+﻿using ArbitraryArtifactDetector.Service;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System;
 using System.ServiceProcess;
 using System.Windows.Forms;
@@ -18,19 +16,7 @@ namespace ArbitraryArtifactDetector
         [STAThread]
         private static int Main(string[] args)
         {
-            ArtifactConfiguration config = JsonConvert.DeserializeObject<ArtifactConfiguration>(@"
-    {
-        detectors: {0: 'OpenWindowDetector;none;none', 1: 'VisualFeatureDetector;none;none'},
-        artifact_name: 'test',
-        'detection_interval': 5000,
-        runtime_information: {
-            artifact_name: 'yeeee',
-            process_names: 'test1|test2',
-            reference_images: 'C:\\Users\\Felix\\source\\repos\\recipes\\01_Jibberish-Mittel\\screenshot'
-        }
-    }
-");
-            /*ServiceBase[] ServicesToRun;
+            ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
                 new DetectorService()
@@ -47,7 +33,7 @@ namespace ArbitraryArtifactDetector
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 #endif
-*/
+
             return 0;
         }
     }
